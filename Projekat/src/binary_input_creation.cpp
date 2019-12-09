1d84b927f2a5e5b111ee47e6bbcda9f667ae1c07
Dejan Todorovic <deja99@live.com>
2019-12-09 14:59:33 +0100

    Finalna verzija projekta :D (nadam se)

--- Projekat/src/binary_input_creation.cpp
+++ Projekat/src/binary_input_creation.cpp
@@ -1,33 +1,33 @@
-ofstream myFile("./res/input_bin.bin", ios::out | ios::binary);
-
-		Student s("Dejan", "Todorovic", "sw17/2018");
-		std::vector<int> v1 = { 100, 95, 92, 100, 94, 99 };
-		std::vector<int> v2 = { 100, 98, 90, 95 };
-		std::vector<int> v3 = { 70, 100, 88, 80, 100, 92, 96, 100, 89, 93 };
-
-		s.write_to_binary_file(myFile);
-		myFile.write((char*)&v1[0], 6 * sizeof(int));
-		myFile.write((char*)&v2[0], 4 * sizeof(int));
-		myFile.write((char*)&v3[0], 10 * sizeof(int));
-
-		Student s1("Milos", "Panic", "sw19/2018");
-		std::vector<int> v1v1 = { 100, 94, 100, 95, 97, 89 };
-		std::vector<int> v1v2 = { 100, 97, 100, 100 };
-		std::vector<int> v1v3 = { 80, 90, 86, 82, 99, 100, 94, 84, 100, 91 };
-
-		s1.write_to_binary_file(myFile);
-		myFile.write((char*)&v1v1[0], 6 * sizeof(int));
-		myFile.write((char*)&v1v2[0], 4 * sizeof(int));
-		myFile.write((char*)&v1v3[0], 10 * sizeof(int));
-
-		Student s2("Zoran", "Jankov", "sw08/2018");
-		std::vector<int>v12 = { 98, 91, 40, 95, 97, 89 };
-		std::vector<int>v22 = { 64, 97, 81, 100 };
-		std::vector<int>v32 = { 80, 92, 86, 85, 92, 91, 94, 85, 90, 91 };
-
-		s2.write_to_binary_file(myFile);
-		myFile.write((char*)&v12[0], 6 * sizeof(int));
-		myFile.write((char*)&v22[0], 4 * sizeof(int));
-		myFile.write((char*)&v32[0], 10 * sizeof(int));
-
-		myFile.close();
+//ofstream myfile("./res/input_bin.bin", ios::out | ios::binary);
+//
+//		student s("dejan", "todorovic", "sw17/2018");
+//		std::vector<int> v1 = { 100, 95, 92, 100, 94, 99 };
+//		std::vector<int> v2 = { 100, 98, 90, 95 };
+//		std::vector<int> v3 = { 70, 100, 88, 80, 100, 92, 96, 100, 89, 93 };
+//
+//		s.write_to_binary_file(myfile);
+//		myfile.write((char*)&v1[0], 6 * sizeof(int));
+//		myfile.write((char*)&v2[0], 4 * sizeof(int));
+//		myfile.write((char*)&v3[0], 10 * sizeof(int));
+//
+//		student s1("milos", "panic", "sw19/2018");
+//		std::vector<int> v1v1 = { 100, 94, 100, 95, 97, 89 };
+//		std::vector<int> v1v2 = { 100, 97, 100, 100 };
+//		std::vector<int> v1v3 = { 80, 90, 86, 82, 99, 100, 94, 84, 100, 91 };
+//
+//		s1.write_to_binary_file(myfile);
+//		myfile.write((char*)&v1v1[0], 6 * sizeof(int));
+//		myfile.write((char*)&v1v2[0], 4 * sizeof(int));
+//		myfile.write((char*)&v1v3[0], 10 * sizeof(int));
+//
+//		student s2("zoran", "jankov", "sw08/2018");
+//		std::vector<int>v12 = { 98, 91, 40, 95, 97, 89 };
+//		std::vector<int>v22 = { 64, 97, 81, 100 };
+//		std::vector<int>v32 = { 80, 92, 86, 85, 92, 91, 94, 85, 90, 91 };
+//
+//		s2.write_to_binary_file(myfile);
+//		myfile.write((char*)&v12[0], 6 * sizeof(int));
+//		myfile.write((char*)&v22[0], 4 * sizeof(int));
+//		myfile.write((char*)&v32[0], 10 * sizeof(int));
+//
+//		myfile.close();
--- ReadMe.txt
+++ ReadMe.txt
@@ -9,18 +9,18 @@ Za izradu projekta je korišćen Microsoft Visual Studio 2019
 Lista ispitnih datoteka:
     Datoteke sa dobrim sadržajem:
         input.txt - datoteka sa validnim podacima (za testiranje funkcionalnosti programa)
-        input.bin - binarna datoteka sa validnim podacima (za testiranje funkcionalnosti programa)
+        input_bin.bin - binarna datoteka sa validnim podacima (za testiranje funkcionalnosti programa)
 
     Datoteke sa lošim sadržajem:
         input_bad_student1.txt - datoteka koja sadrži sadrži više polja za studente nego što je potrebno
         input_bad_student2.txt - datoteka koja sadrži sadrži manje polja za studente nego što je potrebno
         input_bad_grades.txt - datoteka koja sadrži višak ocena
+        input_bad_grades2.txt - datoteka koja sadrži slovo umesto ocene
         input_bad_range.txt - datoteka koja sadrži ocene koje nisu u opsegu [0-100]
-        input_bad_student1.bin - binarna datoteka koja sadrži sadrži više polja za studente nego što je potrebno
-        input_bad_student2.bin - binarna datoteka koja sadrži sadrži manje polja za studente nego što je potrebno
-        input_bad_grades.bin - binarna datoteka koja sadrži višak ocena
-        input_bad_range.bin - binarna datoteka koja sadrži ocene koje nisu u opsegu [0-100]
+        input_bin_bad_student1.bin - binarna datoteka koja sadrži sadrži više polja za studente nego što je potrebno
+        input_bin_bad_student2.bin - binarna datoteka koja sadrži sadrži manje polja za studente nego što je potrebno
+        input_bin_bad_grades.bin - binarna datoteka koja sadrži višak ocena
+        input_bin_bad_range.bin - binarna datoteka koja sadrži ocene koje nisu u opsegu [0-100]
 
     Datoteke sa nepotpunim sadržajem (program se oporavlja):
         input_grades.txt - datoteka koja ne sadrži dovoljan broj ocena (dodaju se nule i program nastavlja sa radom)
-        input_grades.bin - binarna datoteka koja ne sadrži dovoljan broj ocena (dodaju se nule i program nastavlja sa radom)
